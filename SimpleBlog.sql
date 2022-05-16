233ab8ad01046ac513db796c7c487988a28ed676
赵睿宇 <1447051936@qq.com>
2022-05-16 06:28:19 +0000

    update SimpleBlog.sql.

--- SimpleBlog.sql
+++ SimpleBlog.sql
@@ -15,6 +15,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE SCHEMA `simpleblog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+use simpleblog;
 --
 -- Table structure for table `t_blog`
 --
